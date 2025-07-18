bc5676657538d5c0940bbed9147f85f33caf8343
NGuy2919 <aekmongkhon.p@kkumail.com>
2025-07-18 14:54:39 +0700

    2add

--- Assignment/Lab4_BVT/MembershipRanking/src/sqa/test/RobustnessMembership.java
+++ Assignment/Lab4_BVT/MembershipRanking/src/sqa/test/RobustnessMembership.java
@@ -1,14 +1,142 @@
 package sqa.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class RobustnessMembership {
+import sqa.main.Ranking;
+
+public class RobustnessMembership {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testTC014() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 0, 550);
+		assertEquals("Standard",res);
 	}
 
+	@Test
+	public void testTC015() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 1, 550);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC016() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 2, 550);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC017() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 4, 550);
+		assertEquals("Gold",res);
+	}
+
+	@Test
+	public void testTC018() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 6, 550);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC019() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 7, 550);
+		assertEquals("Standard",res);
+	}
+	@Test
+	public void testTC020() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 8, 550);
+		assertEquals("Standard",res);
+	}
+
+	@Test
+	public void testTC021() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 4, 99);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC022() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 4, 100);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC023() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 4, 101);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC024() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 4, 999);
+		assertEquals("Gold",res);
+	}
+	
+	@Test
+	public void testTC025() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 4, 1000);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC026() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(55000, 4, 1001);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC027() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(9999, 4, 550);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC028() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(10000, 4, 550);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC029() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(10001, 4, 550);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC030() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(99999, 4, 550);
+		assertEquals("Gold",res);
+	}
+	
+	@Test
+	public void testTC031() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(100000, 4, 550);
+		assertEquals("Standard",res);
+	}
+	
+	@Test
+	public void testTC032() {
+		Ranking rank = new Ranking();
+		String res = rank.CalculateMembershipRank(100001, 4, 550);
+		assertEquals("Standard",res);
+	}
 }
